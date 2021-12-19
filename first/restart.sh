c9cde51b1db1f6831ffcc4c91bece11a4d8359ff
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2021-12-19 20:58:16 +0900

    Add files via upload

--- first/restart.sh
+++ first/restart.sh
@@ -3,4 +3,5 @@ cp ~/.config/user-dirs.dirs ~/
 #cp /etc/bluetooth/main.conf ~/
 cat ~/.bashrc > bashrc
 cp ~/.config/xfce4/terminal/terminalrc ~/
+cp ~/.vimrc ~/vimrc
 #sudo cp -r ~/* /media/kenter/*/
